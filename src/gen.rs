33b773da938d0a9a9a8c3cc3cb199573556f44fa
brianschnee <brian.a.schnee@gmail.com>
2023-08-30 13:07:15 -0400

    reorder functions

--- src/gen.rs
+++ src/gen.rs
@@ -1,19 +1,6 @@
 use std::{fs::File, path::Path, io::Write};
 use super::args::{Entity, StructProperties};
 
-pub fn generate_code(name: String, template: String) -> () {
-    let output_dir = "dist";
-
-    // Create the output directory if it doesn't exist
-    if !Path::new(output_dir).exists() {
-        std::fs::create_dir(output_dir).expect("Failed to create output directory");
-    }
-
-    let filename = format!("{}/{}.rs", output_dir, name);
-    let mut file = File::create(&filename).expect("Failed to create file");
-    file.write_all(template.as_bytes()).expect("Failed to write to file");
-}
-
 pub fn create_template(entity: Entity) -> (String, String) {
     let (name, template) = match entity {
         Entity::Struct(props) => (props.name.clone(), create_struct(props)),
@@ -38,4 +25,17 @@ impl {0} {{
 "#,
         props.name
     )
+}
+
+pub fn generate_code(name: String, template: String) -> () {
+    let output_dir = "dist";
+
+    // Create the output directory if it doesn't exist
+    if !Path::new(output_dir).exists() {
+        std::fs::create_dir(output_dir).expect("Failed to create output directory");
+    }
+
+    let filename = format!("{}/{}.rs", output_dir, name);
+    let mut file = File::create(&filename).expect("Failed to create file");
+    file.write_all(template.as_bytes()).expect("Failed to write to file");
 }
